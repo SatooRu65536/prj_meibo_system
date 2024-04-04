0f50705102544b5a2224ae92f7c52f8e4c57e789
satooru65536 <ryuto4619@gmail.com>
2024-04-04 16:57:49 +0900

    fix: 招待用QRコードを変更

--- site/app/signup/_components/page/Signup.tsx
+++ site/app/signup/_components/page/Signup.tsx
@@ -6,7 +6,11 @@ export default function SignupPage() {
   return (
     <main className={styles.signup}>
       <div className={styles.container}>
-        <QRCode className={styles.qrcode} value="https://satooru.me" />
+        <QRCode
+          className={styles.qrcode}
+          value="https://meibo.sysken.net/signup"
+        />
+        <p className={styles.desc}>招待用のQRコードです</p>
 
         <a href={ROUTES.registration.path} className={styles.registration}>
           登録に進む
